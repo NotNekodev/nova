a427b298e76c030794b8993c074e91b7b99dcd61
lolguy91 <retek1544@gmail.com>
2025-12-13 19:16:32 +0100

    asset error handling fix

--- src/shared/asset.rs
+++ src/shared/asset.rs
@@ -21,7 +21,13 @@ pub fn get_asset(shared: &SharedData,name: &str) -> Asset {
     let (reply_tx, reply_rx) = mpsc::channel::<Asset>();
 
     //send command
-    shared.asset_tx.send(AssetRequest { name: name.to_string(), sender: reply_tx });
+    match shared.asset_tx.send(AssetRequest { name: name.to_string(), sender: reply_tx }) {
+        Err(e) => {
+            err!(shared, "Failed to send Asset request \"{name}\": {e}");
+            return Asset::None
+        },
+        _ => ()
+    };
 
     return reply_rx.recv().unwrap_or_else(|e| {
         err!(shared, "Failed to load Asset \"{name}\": {e}");
